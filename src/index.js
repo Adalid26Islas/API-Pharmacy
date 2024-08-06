bdb969cd1f9f6f775685453045c7ef4cedd05edc
Adalid26Islas <adalidislasquintero@gmail.com>
2024-08-06 14:58:02 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import app from "./app";
+import app from "./app.js";
+app.get("/getAll", (req, res) => {
+    res.json({message: "Hola desde la maquina de Adalid"})
+})
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
