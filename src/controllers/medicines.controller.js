5efebe6217d83b7dc10423ade7f7f54c9b34886d
Adalid26Islas <adalidislasquintero@gmail.com>
2024-08-16 16:08:54 -0600

    Creación de la funcion para encontrar solo un elemento

--- src/controllers/medicines.controller.js
+++ src/controllers/medicines.controller.js
@@ -12,9 +12,22 @@ export const getAll = async (req, res) =>{
 export const insertMedicine = async (req, res) => {
     await medicinesDAO.insertMedicine(req.body)
     .then((medicine) =>{
-        res.json(medicine);
+        res.json(medicine)
     })
     .catch((err)=> {
         res.json({message:err});
     });
+}
+
+export const getOne = async (req, res) =>{
+    await medicine.getOne(req.params.barcode)
+    .then((medicine) => {
+        if(medicine !=null)
+            res.json(medicine)
+        else
+        res.json({message:"Medicina no encontrada"});
+    })
+    .catch(err =>{
+        res.json({message:err});
+    })
 }
--- src/daos/medicines.dao.js
+++ src/daos/medicines.dao.js
@@ -1,3 +1,4 @@
+import { resolveObjMapThunk } from "graphql";
 import Medicine from "../models/Medicine.js";
 
 const medicinesDAO ={};
@@ -8,4 +9,8 @@ medicinesDAO.insertMedicine = async (medicine) => {
     return await Medicine.create(medicine)
 }
 
+//Recibe como parametro el barcode y con el findOne muestra el elemento que exista en la coleccion, si no existe devolvera null
+medicinesDAO.getOne = async(barcode) =>{
+    return await Medicine.findOne({barcode:barcode})
+}
 export default medicinesDAO;
